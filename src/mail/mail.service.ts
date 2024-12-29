0e60461faa4f5ee2702788c0201cd857dd053f97
luizleal <luizandreleal100@gmail.com>
2024-12-29 20:47:44 -0300

    Send email system switched to another lib

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,103 +1,30 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import * as nodemailer from 'nodemailer';
+import emailjs from '@emailjs/nodejs';
 
 @Injectable()
 export class MailService {
-   private transporter: nodemailer.Transporter;
-
-   constructor() {
-      this.transporter = nodemailer.createTransport({
-         host: process.env.SMTP_HOST,
-         port: Number(process.env.SMTP_PORT),
-         secure: true,
-         auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS,
-         },
-      });
-   }
-
    async sendResetPasswordEmail(
       email: string,
       username: string,
       token: string,
-      attemps = 0,
-      maxAttemps = 10,
    ) {
       try {
-         const emailResult = await this.transporter.sendMail({
-            from: `Tabuada do Glécio <${process.env.SMTP_USER}>`,
-            to: email,
-            subject: 'Pedido de redefinição de senha',
-            html: `
-                <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
-                  style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
-                  <tr>
-                        <td>
-                           <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
-                              align="center" cellpadding="0" cellspacing="0">
-                              <tr>
-                                    <td style="height:80px;">&nbsp;</td>
-                              </tr>
-                              <tr>
-                                    <td style="height:20px;">&nbsp;</td>
-                              </tr>
-                              <tr>
-                                    <td>
-                                       <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
-                                          style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
-                                          <tr>
-                                                <td style="height:40px;">&nbsp;</td>
-                                          </tr>
-                                          <tr>
-                                                <td style="padding:0 35px;">
-                                                   <h6 style="color:#1e1e2d; font-weight:300; margin-top:0; margin-bottom:20px;font-size:26px;font-family:'Rubik',sans-serif;">Olá, ${username}</h6>
-                                                   <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Pedido de redefinição de senha</h1>
-                                                   <span
-                                                      style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
-                                                   <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">Não podemos simplesmente enviar a sua senha antiga. Um link exclusivo para redefinir seu a senha foi gerada para você. Para redefinir sua senha, clique no botão link abaixo e siga as instruções.
-                                                   </p>
-                                                   <a href="${process.env.SITE_URL}/password-reset/confirm/${token}"
-                                                      style="background:#7B4EC7;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Resetar senha</a>
-                                                </td>
-                                          </tr>
-                                          <tr>
-                                                <td style="height:40px;">&nbsp;</td>
-                                          </tr>
-                                       </table>
-                                    </td>
-                              <tr>
-                                    <td style="height:20px;">&nbsp;</td>
-                              </tr>
-                              <tr>
-                                    <td style="height:80px;">&nbsp;</td>
-                              </tr>
-                           </table>
-                        </td>
-                  </tr>
-               </table>`,
-         });
-         console.log('Email send sucessfully!');
-         console.log(emailResult);
-
-         if (!emailResult.response.includes('OK')) {
-            if (attemps <= maxAttemps) {
-               console.warn(
-                  `Email send failure. Trying again ${attemps + 1} times`,
-               );
-               await this.sendResetPasswordEmail(
-                  email,
-                  username,
-                  token,
-                  attemps + 1,
-                  maxAttemps,
-               );
-            } else {
-               throw new InternalServerErrorException(
-                  'Falha ao enviar email. Tente novamente mais tarde.',
-               );
-            }
+         const templateOptions = {
+            username: username,
+            to_email: email,
+            site_url: process.env.SITE_URL,
+            token: token
          }
+
+         const response = await emailjs.send(
+            'tabuada_email',
+            'reset_password',
+            templateOptions,
+            {
+               publicKey: process.env.EMAILJS_PUBLIC_KEY,
+               privateKey: process.env.EMAILJS_PRIVATE_KEY,
+            },
+         );
       } catch (err) {
          console.error(err);
          throw err;
