f52310c76b98e0dd77f991a5f42e6ff2313b19c0
luizleal <luizandreleal100@gmail.com>
2025-01-11 20:55:57 -0300

    Cors config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
    const app = await NestFactory.create(AppModule);
+
+   app.enableCors({
+      origin: process.env.SITE_URL,
+      methods: 'GET,POST,PUT,DELETE',
+      allowedHeaders: 'Content-Type, Authorization',
+   });
+
    await app.listen(3000);
 }
 bootstrap();
