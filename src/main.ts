493eb4e80a8da8b435aa6556490dd9ade638eef5
luizleal <luizandreleal100@gmail.com>
2025-01-21 10:13:32 -0300

    Cors origin change to devlopment mode

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
    const app = await NestFactory.create(AppModule);
 
    app.enableCors({
-      origin: process.env.SITE_URL,
+      origin: '*',
       methods: 'GET,POST,PUT,DELETE',
       allowedHeaders: 'Content-Type, Authorization',
    });
