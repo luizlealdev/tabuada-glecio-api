01bd3a6a1ee60f92f07639ff6b7df3b51131d2f7
luizleal <luizandreleal100@gmail.com>
2025-01-21 10:22:26 -0300

    Vercel.json cors config

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,9 @@ async function bootstrap() {
    app.enableCors({
       origin: '*',
       methods: 'GET,POST,PUT,DELETE',
-      allowedHeaders: 'Content-Type, Authorization',
+      credentials: true,
+      allowedHeaders:
+         'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
    });
 
    await app.listen(3000);
